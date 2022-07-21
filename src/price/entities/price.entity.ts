aa720bc6f44f487b378f37f6ef3991216c8cc157
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-07-21 22:05:42 +0200

    feat: increase paginate performance
    Create index for updatedAt column

--- src/price/entities/price.entity.ts
+++ src/price/entities/price.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryColumn } from 'typeorm';
 
 @Entity()
+@Index(['updatedAt'])
 export class Price {
   @PrimaryColumn()
   sku: string;
